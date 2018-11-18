531a653478600202345a0b4e482e355080fc85a9
Raphael Diniz <raphaeldinizrp@gmail.com>
2018-11-18 07:07:17 -0300

    minor directory changes

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 import LoginView from './src/View/Login/LoginView';
+import { ImageBackground } from 'react-native';
+import styles from './src/Styles/styles';
+
+const backgroundImage = require('./assets/imgs/background.jpg')
 
 export default class App extends Component {
+  
   render() {
     return (
-      <LoginView />
+      <ImageBackground
+        source={backgroundImage}
+        style={styles.backgroundImage}
+      >
+        <LoginView />
+      </ImageBackground>
+      
     );
   }
 }
--- src/View/Login/LoginView.js
+++ src/View/Login/LoginView.js
@@ -1,21 +1,13 @@
-import { View, ImageBackground, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import React, { Component } from 'react';
 import TabViewLogin from '../../Components/Login/TabViewLogin';
-import styles from '../../Styles/styles';
-
-const backgroundImage = require('../../../assets/imgs/background.jpg')
 
 export default class LoginView extends Component {
     render() {
       return (
-          <ImageBackground
-            source={backgroundImage}
-            style={styles.backgroundImage}
-          >
-           <View style={styles.tabView}>
-            <TabViewLogin />
-           </View>
-          </ImageBackground>
+        <View style={styles.tabView}>
+          <TabViewLogin />
+        </View>
       );
     }
 
